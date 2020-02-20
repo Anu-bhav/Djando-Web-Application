cd86427c1bde3e66309677591f9ac293650da8c0
Anu-Bhav <myster.anubhav@gmail.com>
2020-02-20 23:35:50 +0000

    Templates

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,37 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 # Create your views here.
+
+posts = [
+    {
+        'author': 'Anubhav',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'Febuary 27, 2020'
+    },
+    {
+        'author': 'Anubhav',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'Febuary 28, 2020'
+    },
+    {
+        'author': 'Anubhav',
+        'title': 'Blog Post 3',
+        'content': 'Third post content',
+        'date_posted': 'Febuary 29, 2020'
+    },
+]
+
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    # passing posts which is a list[] of dict{} containing multiple posts
+    context = {
+        'posts': posts
+    }
+    # return HttpResponse('<h1>Blog Home</h1>')
+    return render(request, "blog/home.html", context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, "blog/about.html", {'title': 'About'})
