eea66702454ee3e34c0445fada7cd99f0dd3f179
Anu-Bhav <myster.anubhav@gmail.com>
2020-02-20 22:06:59 +0000

    Applications and Routes

--- python_django/urls.py
+++ python_django/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
+# Always put trailing slash at the end
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('blog/', include('blog.urls')),
+    # make the blog home as the default home page of the website
+    path('', include('blog.urls')),
+
 ]
