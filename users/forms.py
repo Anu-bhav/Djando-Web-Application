d4b7f0f0f8a854466ab849c198607ddc2e4cb894
Anu-Bhav <myster.anubhav@gmail.com>
2020-02-21 23:40:16 +0000

    Update User Profile

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -10,4 +11,15 @@ class UserRegisterForm(UserCreationForm):
         # within the config, the model affected is the User model
         model = User
         # fields in the list are the fields that we want on the form and are in that specific order
-        fields = [ 'username', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2']
+        
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
