2ad2653a6902af5a202f5b04397c62beef4a664f
Anu-Bhav <myster.anubhav@gmail.com>
2020-02-21 05:33:40 +0000

    Database and Migrations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    # Don't put parenthesis timezone.now() as we do not want it to be executed at that point
+    # we just want to pass the function name as the default value so that it knows what to execute later
+    date_posted = models.DateTimeField(default=timezone.now)
+    # when a user is deleted, the post associated to that user will be deleted too with on_delete=models.CASCADE
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,34 +1,32 @@
 from django.shortcuts import render
+from .models import Post # . means in current directory
 #from django.http import HttpResponse
 
 # Create your views here.
 
-posts = [
-    {
-        'author': 'Anubhav',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'Febuary 27, 2020'
-    },
-    {
-        'author': 'Anubhav',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'Febuary 28, 2020'
-    },
-    {
-        'author': 'Anubhav',
-        'title': 'Blog Post 3',
-        'content': 'Third post content',
-        'date_posted': 'Febuary 29, 2020'
-    },
-]
+# posts = [
+#     {
+#         'author': 'Anubhav',
+#         'title': 'Blog Post 1',
+#         'content': 'First post content',
+#         'date_posted': 'Febuary 27, 2020'
+#     },
+#     {
+#         'author': 'Anubhav',
+#         'title': 'Blog Post 2',
+#         'content': 'Second post content',
+#         'date_posted': 'Febuary 28, 2020'
+#     }
+# ]
 
 
 def home(request):
     # passing posts which is a list[] of dict{} containing multiple posts
+    # context = {
+    #     'posts': posts
+    # }
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     # return HttpResponse('<h1>Blog Home</h1>')
     return render(request, "blog/home.html", context)
