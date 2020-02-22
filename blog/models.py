d365debecf34e9e3bd7a15729f033b488ee18de9
Anu-Bhav <myster.anubhav@gmail.com>
2020-02-22 00:48:32 +0000

    Create, Update, and Delete Posts

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 class Post(models.Model):
@@ -14,3 +15,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    # for django to find the location of a post
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 from .models import Post # . means in current directory
 #from django.http import HttpResponse
 
@@ -31,5 +39,57 @@ def home(request):
     # return HttpResponse('<h1>Blog Home</h1>')
     return render(request, "blog/home.html", context)
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'  # <app><model>_<viewtype>.html
+    context_object_name = 'posts'  # same name as context in home function as home.html is expecting 'posts'
+    ordering = ['-date_posted']     # default is asc but adding the - before makes it order in desc
+
+class PostDetailView(DetailView):
+    # <app>/<model>_<viewtype>.html
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    # <app>/<model>_<viewtype>.html
+    # create and update have same page, default naming convention is <app>/<model>_form.html
+    # blog/post_form.html
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user    # we are setting the author id here before the form is save by overridding the form_valid function
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):    # the mixins must always be passed first
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user    # we are setting the author id here before the form is save by overridding the form_valid function
+        return super().form_valid(form)
+    
+    # prevent other users from editing another user's post
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    # <app>/<model>_<viewtype>.html
+    model = Post
+    success_url = "/"
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+    
+
 def about(request):
     return render(request, "blog/about.html", {'title': 'About'})
+
+
